8f905f9f06b5146b5e107de34b674d3a1dcc2d90
Alaric Henrot <alaric.henrot@ospi.fr>
2024-08-09 11:56:06 +0200

    add relation between categories and programs and add fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -21,10 +21,11 @@ class CategoryFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        foreach (Self::CATEGORIES as $key => $categoryName) {
+        foreach (Self::CATEGORIES as $categoryName) {
             $category = new Category();
             $category->setName($categoryName);
             $manager->persist($category);
+            $this->addReference('category_'.$categoryName, $category);
         }
         $manager->flush();
     }
---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture
+{
+    const PROGRAMS = [
+        [
+            'title' => 'Walking Dead',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 2',
+            'synopsis' => 'some texte',
+            'category' => 'category_Horreur',
+        ],
+        [
+            'title' => 'Titre 3',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Aventure',
+        ],
+        [
+            'title' => 'Titre 4',
+            'synopsis' => 'some texte',
+            'category' => 'category_Horreur',
+        ],
+        [
+            'title' => 'Titre 5',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 6',
+            'synopsis' => 'some texte',
+            'category' => 'category_Horreur',
+        ],
+        [
+            'title' => 'Titre 7',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 8',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 9',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Aventure',
+        ],
+        [
+            'title' => 'Titre 10',
+            'synopsis' => 'some texte',
+            'category' => 'category_Fantastique',
+        ],
+        [
+            'title' => 'Titre 11',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 12',
+            'synopsis' => 'some texte',
+            'category' => 'category_Animation',
+        ],
+        [
+            'title' => 'Titre 13',
+            'synopsis' => 'palu menfou.',
+            'category' => 'category_Aventure',
+        ],
+        [
+            'title' => 'Titre 14',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 15',
+            'synopsis' => 'palu menfou',
+            'category' => 'category_Fantastique',
+        ],
+        [
+            'title' => 'Titre 16',
+            'synopsis' => 'some texte',
+            'category' => 'category_Animation',
+        ],
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::PROGRAMS as $programDefinition) {
+            $program = new Program();
+            $program->setTitle($programDefinition['title']);
+            $program->setSynopsis($programDefinition['synopsis']);
+            $program->setCategory($this->getReference($programDefinition['category']));
+            $manager->persist($program);
+        }
+
+        $manager->flush();
+    }
+    public function getDependencies() {
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+}
